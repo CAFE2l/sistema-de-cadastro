ccff1163a2e6843f3942ef79059d6729aaaa8c8b
CAFÉ <pciskolargx@gmail.com>
2025-06-19 16:13:48 +0000

    test

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 console.log("rodando o sistema de gerenciamento de jogos");
+console.log("aplicação está se conectado ao  banco de dados");
+// mais um console log de test:
+console.log("O paul fez esse commit sem voce ver na aula!");
+console.log("odeio git");
