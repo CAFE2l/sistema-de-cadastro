e085c5b0f12c59ecbbe6c3bf337207906b8c73d4
CAFÉ <pciskolargx@gmail.com>
2025-06-19 16:32:59 +0000

    corrigido

--- app.js
+++ app.js
@@ -3,3 +3,4 @@ console.log("aplicação está se conectado ao  banco de dados");
 // mais um console log de test:
 console.log("O paul fez esse commit sem voce ver na aula!");
 console.log("odeio git");
+//tudo corrigido por aq
